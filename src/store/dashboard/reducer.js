f0c423cfc090c3e451c88db764a9b471bfe10513
Danil Lytvyn <danil.googlle@gmail.com>
2022-09-08 00:37:21 +0300

    modify dashboard reducer

--- src/store/dashboard/reducer.js
+++ src/store/dashboard/reducer.js
@@ -1,18 +1,55 @@
 import { DASHBOARD_LOADED } from "./actions";
 import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 
+const ADD_LIST = "ADD_LIST";
+const DELETE_LIST = "DELETE_LIST";
+const SET_TODAY = "SET_TODAY";
+
 function openedTasksReducer(state = {}, action) {
-  if (action.type === DASHBOARD_LOADED) {
-    let lists = action.payload.lists.map((list) => [list.id, Number(list.undone)]);
-    return Object.fromEntries(lists);
-  } else {
-    return state;
+  switch (action.type) {
+    case DASHBOARD_LOADED:
+      let lists = action.payload.lists.map((list) => [list.id, Number(list.undone)]);
+      return Object.fromEntries(lists);
+    case "SET_TODAY":
+      return 1;
+    default:
+      return state;
   }
 }
 
+function todayReducer(state = 0, { type, payload }) {
+  switch (type) {
+    case DASHBOARD_LOADED:
+      return payload.today;
+    case SET_TODAY:
+      return payload;
+    default:
+      return state;
+  }
+}
+
+function listsReducer(state = [], { type, payload }) {
+  switch (type) {
+    case DASHBOARD_LOADED:
+      return payload.today;
+    case ADD_LIST:
+      return [...state, payload];
+    case DELETE_LIST:
+      return [...state.filter((list) => list.id !== payload)];
+    default:
+      return state;
+  }
+}
+
+// export default combineReducers({
+//   today: (today = 0, { type, payload }) => (type === DASHBOARD_LOADED ? payload.today : today),
+//   lists: (lists = [], { type, payload }) => (type === DASHBOARD_LOADED ? payload.lists : lists),
+//   openedTasks: openedTasksReducer,
+// });
+
 export default combineReducers({
-  today: (today = 0, { type, payload }) => (type === DASHBOARD_LOADED ? payload.today : today),
-  lists: (lists = [], { type, payload }) => (type === DASHBOARD_LOADED ? payload.lists : lists),
+  today: todayReducer,
+  lists: listsReducer,
   openedTasks: openedTasksReducer,
 });
 
